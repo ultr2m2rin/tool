5b650abdc018534602778a537f98db621edead36
Sereda <nigeosha@gmail.com>
2022-04-09 14:43:10 -0400

    routes WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
 import Home from './home/index'
+import Quiz from './quiz';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter >
+        <Routes>
+          <Route path="/quiz">
+            <Quiz />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </BrowserRouter >
     </div>
   );
 }
