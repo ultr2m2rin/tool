f87a6b4f2210a74b0bbd5fbda2d1c140994f1b16
ultr2m2rin <37417288+ultr2m2rin@users.noreply.github.com>
2022-04-09 15:15:48 -0400

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
 import Home from './home/index'
+import Setup from './setup';
+
+export enum RouteNames {
+  setup = 'setup'
+}
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={`/${RouteNames.setup}`} element={<Setup />} />
+        </Routes>
+      </BrowserRouter >
     </div>
   );
 }
