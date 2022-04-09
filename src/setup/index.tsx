65a32c233e9a9c5ece754a82b163ed4205a067ad
Sereda <nigeosha@gmail.com>
2022-04-09 19:09:55 -0400

    adding score

--- src/setup/index.tsx
+++ src/setup/index.tsx
@@ -39,109 +39,170 @@ export default function Setup() {
   // TODO: investigate form frameworks if there's a better way to manage conditional rendering of questions
   const [questionOneValue, setQuestionOneValue] = useState<string>("");
   const [questionEightValue, setQuestionEightValue] = useState<string>("");
+  const [score, setScore] = useState(0);
+  const [total, setTotal] = useState(8);
+
+  const answerQuestionOne = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestionOneValue(event.target.value);
+    countScore(event);
+
+    if (event.target.value == "no") setTotal(5);
+  };
+
+  const countScore = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    switch (name) {
+      case "question-one": {
+        if (value === "yes") setScore(score + 1);
+        break;
+      }
+      case "question-two": {
+        if (value === "no") setScore(score + 1);
+        break;
+      }
+      case "question-three": {
+        if (value === "no") setScore(score + 1);
+        break;
+      }
+      case "question-four": {
+        if (value === "yes") setScore(score + 1);
+        break;
+      }
+      case "question-five": {
+        if (value === "yes") setScore(score + 1);
+        break;
+      }
+      case "question-six": {
+        if (value === "yes") setScore(score + 1);
+        break;
+      }
+      case "question-seven": {
+        if (value === "no") setScore(score + 1);
+        break;
+      }
+      case "question-nine": {
+        if (value === "yes") setScore(score + 1);
+        break;
+      }
+      default: {
+        return 0;
+      }
+    }
+  };
 
   return (
-    <div>
-      <Question
-        title={QuestionOneTitle}
-        name="question-one"
-        optionOne={<QuestionOneOptionOne />}
-        optionTwo={<QuestionOneOptionTwo />}
-        onChange={(event) => setQuestionOneValue(event.target.value)}
-      />
-
-      {questionOneValue === "yes" && (
-        <>
-          <Question
-            title={QuestionTwoTitle}
-            name="question-two"
-            optionOne={<QuestionTwoOptionOne />}
-            optionTwo={<QuestionTwoOptionTwo />}
-          />
-          <Question
-            title={QuestionThreeTitle}
-            name="question-three"
-            optionOne={<QuestionThreeOptionOne />}
-            optionTwo={<QuestionThreeOptionTwo />}
-            optionThree={<QuestionThreeOptionThree />}
-            optionThreeText="Prefer not to answer"
-          />
-          <Question
-            title={QuestionFourTitle}
-            name="question-four"
-            optionOne={<QuestionFourOptionOne />}
-            optionTwo={<QuestionFourOptionTwo />}
-          />
-          <Question
-            title={QuestionFiveTitle}
-            name="question-five"
-            optionOne={<QuestionFiveOptionOne />}
-            optionTwo={<QuestionFiveOptionTwo />}
-          />
-          <Question
-            title={QuestionSixTitle}
-            name="question-six"
-            optionOne={<QuestionSixOptionOne />}
-            optionTwo={<QuestionSixOptionTwo />}
-            optionThree={<QuestionSixOptionThree />}
-            optionThreeText="I am not familiar with these"
-          />
-          <Question
-            title={QuestionSevenTitle}
-            name="question-seven"
-            optionOne={<QuestionSevenOptionOne />}
-            optionTwo={<QuestionSevenOptionTwo />}
-            optionThree={<QuestionSevenOptionThree />}
-            optionThreeText="Prefer not to answer"
-          />
-          <Question
-            title={QuestionEightTitle}
-            name="question-eight"
-            optionTwo={<QuestionEightOptionTwo />}
-            optionThree={<QuestionEightOptionThree />}
-            optionThreeText="Not applicable"
-            onChange={(event) => setQuestionEightValue(event.target.value)}
-          />
-        </>
-      )}
-
-      {questionOneValue === "no" && (
-        <>
-          <Question
-            title={QuestionSixTitle}
-            name="question-six"
-            optionOne={<QuestionSixOptionOne />}
-            optionTwo={<QuestionSixOptionTwo />}
-            optionThree={<QuestionSixOptionThree />}
-            optionThreeText="I am not familiar with these"
-          />
-          <Question
-            title={QuestionSevenTitle}
-            name="question-seven"
-            optionOne={<QuestionSevenOptionOne />}
-            optionTwo={<QuestionSevenOptionTwo />}
-            optionThree={<QuestionSevenOptionThree />}
-            optionThreeText="Prefer not to answer"
-          />
-          <Question
-            title={QuestionEightTitle}
-            name="question-eight"
-            optionTwo={<QuestionEightOptionTwo />}
-            optionThree={<QuestionEightOptionThree />}
-            optionThreeText="Not applicable"
-            onChange={(event) => setQuestionEightValue(event.target.value)}
-          />
-        </>
-      )}
-
-      {questionEightValue === "yes" && (
+    <>
+      <div>
         <Question
-          title={QuestionNineTitle}
-          name="question-nine"
-          optionOne={<QuestionNineOptionOne />}
-          optionTwo={<QuestionNineOptionTwo />}
+          title={QuestionOneTitle}
+          name="question-one"
+          optionOne={<QuestionOneOptionOne />}
+          optionTwo={<QuestionOneOptionTwo />}
+          onChange={answerQuestionOne}
         />
-      )}
-    </div>
+        {questionOneValue === "yes" && (
+          <>
+            <Question
+              title={QuestionTwoTitle}
+              name="question-two"
+              optionOne={<QuestionTwoOptionOne />}
+              optionTwo={<QuestionTwoOptionTwo />}
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionThreeTitle}
+              name="question-three"
+              optionOne={<QuestionThreeOptionOne />}
+              optionTwo={<QuestionThreeOptionTwo />}
+              optionThree={<QuestionThreeOptionThree />}
+              optionThreeText="Prefer not to answer"
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionFourTitle}
+              name="question-four"
+              optionOne={<QuestionFourOptionOne />}
+              optionTwo={<QuestionFourOptionTwo />}
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionFiveTitle}
+              name="question-five"
+              optionOne={<QuestionFiveOptionOne />}
+              optionTwo={<QuestionFiveOptionTwo />}
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionSixTitle}
+              name="question-six"
+              optionOne={<QuestionSixOptionOne />}
+              optionTwo={<QuestionSixOptionTwo />}
+              optionThree={<QuestionSixOptionThree />}
+              optionThreeText="I am not familiar with these"
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionSevenTitle}
+              name="question-seven"
+              optionOne={<QuestionSevenOptionOne />}
+              optionTwo={<QuestionSevenOptionTwo />}
+              optionThree={<QuestionSevenOptionThree />}
+              optionThreeText="Prefer not to answer"
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionEightTitle}
+              name="question-eight"
+              optionTwo={<QuestionEightOptionTwo />}
+              optionThree={<QuestionEightOptionThree />}
+              optionThreeText="Not applicable"
+              onChange={(event) => setQuestionEightValue(event.target.value)}
+            />
+          </>
+        )}
+        {questionOneValue === "no" && (
+          <>
+            <Question
+              title={QuestionSixTitle}
+              name="question-six"
+              optionOne={<QuestionSixOptionOne />}
+              optionTwo={<QuestionSixOptionTwo />}
+              optionThree={<QuestionSixOptionThree />}
+              optionThreeText="I am not familiar with these"
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionSevenTitle}
+              name="question-seven"
+              optionOne={<QuestionSevenOptionOne />}
+              optionTwo={<QuestionSevenOptionTwo />}
+              optionThree={<QuestionSevenOptionThree />}
+              optionThreeText="Prefer not to answer"
+              onChange={countScore}
+            />
+            <Question
+              title={QuestionEightTitle}
+              name="question-eight"
+              optionTwo={<QuestionEightOptionTwo />}
+              optionThree={<QuestionEightOptionThree />}
+              optionThreeText="Not applicable"
+              onChange={(event) => setQuestionEightValue(event.target.value)}
+            />
+          </>
+        )}
+        {questionEightValue === "yes" && (
+          <Question
+            title={QuestionNineTitle}
+            name="question-nine"
+            optionOne={<QuestionNineOptionOne />}
+            optionTwo={<QuestionNineOptionTwo />}
+            onChange={countScore}
+          />
+        )}
+      </div>
+      score: {score} / {total}
+    </>
   );
 }
